f9e70c3eaf9f6b3f177848dc6561214701f7a06b
Terence S <terence.sacquepee@akawam.com>
2017-01-05 14:35:28 +0100

    __CONFIG__ et Class Person

--- Person.php
+++ Person.php
@@ -1,16 +1,64 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Person
  *
- * @author Quentin
+ * @author osef
  */
-class Person {
-    //put your code here
+class Person 
+{
+    private $_ref;
+    private $_nom;
+    private $_prenom;
+    
+    // Constructeur paramétré
+    function __construct($_ref, $_nom, $_prenom) 
+    {
+        $this->_ref = $_ref;
+        $this->_nom = $_nom;
+        $this->_prenom = $_prenom;
+    }
+    
+    /*          /*
+     *          /*
+     * GETTERS  /*
+     *          /*  
+     *          */
+    
+    function get_ref() 
+    {
+        return $this->_ref;
+    }
+
+    function get_nom() 
+    {
+        return $this->_nom;
+    }
+
+    function get_prenom() 
+    {
+        return $this->_prenom;
+    }
+    
+    /*          /*
+     *          /*
+     * SETTERS  /*
+     *          /*  
+     *          */
+    
+    function set_ref($_ref) 
+    {
+        $this->_ref = $_ref;
+    }
+
+    function set_nom($_nom) 
+    {
+        $this->_nom = $_nom;
+    }
+
+    function set_prenom($_prenom) 
+    {
+        $this->_prenom = $_prenom;
+    }
+
+
 }
