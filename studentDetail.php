617e32b0bfd03d391160cee702590bfd19b454a0
Quentin Helleboid <Quentin@MacBook-Air-de-Quentin.local>
2017-01-06 13:56:55 +0100

    Ajout possibilite ajouter une note

--- studentDetail.php
+++ studentDetail.php
@@ -7,18 +7,48 @@ if (isset($_SESSION['connected_id']) && !empty($_SESSION['connected_id'])) {
     $person = unserialize($_SESSION['connected_objet']);
     $type = get_class($person);
 
-    if ($type == "Teacher" || $type == "Admin") {
+    if ($type == "Teacher") {
+        if (isset($_POST["Ajouter"]) && isset($_POST["note"]) && !empty($_POST["Ajouter"])) {
+            $coeff = !empty($_POST["coeff"]) ? $_POST["coeff"] : "";
+            $comment = !empty($_POST["comment"]) ? $_POST["comment"] : "";
+            $person->insertNote($bdd, intval($_GET["id"]), intval($_POST["note"]), $coeff, $comment);
+            header("Location: " . $_SERVER["REQUEST_URI"]);
+        }
         $note = $person->getAllStudentNotes($bdd, intval($_GET["id"]));
     } else {
         $note = $person->getAllStudentNotes($bdd);
     }
-
-    foreach ($note as $value) {
+    ?>
+    <div id="allNote">
+        <?php
+        foreach ($note as $value) {
+            ?>
+            note : <?= $value["note"] ?> - coeff :  <?= $value["coeff"] ?>   <?= $value["comment"] != "" ? "commentaire" . $value["comment"] : "" ?> <br />
+            <?php
+        }
         ?>
-        note : <?= $value["note"] ?> - coeff :  <?= $value["coeff"] ?>   <?= $value["comment"] != "" ? "commentaire" . $value["comment"] : "" ?> <br />
+    </div>
+    <?php
+    if ($type == "Teacher" || $type == "Admin") {
+        ?>
+        <hr>
+        <div id="ajouterUneNote">
+            <h2>Ajouter une note</h2>
+            <form method="post" action="<?= $_SERVER["REQUEST_URI"] ?>">
+                <label>Note</label>
+                <input type="number" min="0" max="20" name="note" require>
+                <br />
+                <label>Coeff</label>
+                <input type="number" min="0" max="20" name="coeff">
+                <br />
+                <label>Commentaire</label>
+                <input type="text" name="comment">
+                <br /><br />
+                <input type="submit" name="Ajouter">
+            </form>
+        </div>
         <?php
     }
-
     // include("footer.php");
 } else {
     header("Location: index.php");
