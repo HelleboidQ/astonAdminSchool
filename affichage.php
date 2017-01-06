2041bca3e6e0aadd3dfdcde3997889e749cbb8fe
Quentin Helleboid <Quentin@MacBook-Air-de-Quentin.local>
2017-01-06 09:46:24 +0100

    Affiche toute les class

--- affichage.php
+++ affichage.php
@@ -1,13 +1,33 @@
-<div class="buttonleft">
-    <button><a href="Login.html">Retour</a></button>
-    <button><a href="login.html">Deconnexion</a></button>
-</div>
-<div class="center">
-    <img src="img/logo.png" alt="">
-    <h3>Bonjour Monsieur LEGRAND</h3>
-    Bienvenue sur votre espace professeur,<br>
-    Veuillez choisir une classe.
-    <ul>
-        <li><a href="classe.html"> Classe 1</a></li>
-    </ul>
-</div>
+
+<?php
+include("header.php");
+
+if (isset($_SESSION['connected_id']) && !empty($_SESSION['connected_id']) && $_SESSION['connected_type'] == "teacher") {
+    $person = unserialize($_SESSION['connected_objet']);
+    $classRoom = $person->getAllClass($bdd);
+    ?>
+    <div class="buttonleft">
+        <button><a href="Login.html">Retour</a></button>
+        <button><a href="login.html">Deconnexion</a></button>
+    </div>
+    <div class="center">
+        <img src="img/logo.png" alt="">
+        <h3>Bonjour <?= $_SESSION['connected_user'] ?></h3>
+        Bienvenue sur votre espace professeur,<br>
+        Veuillez choisir une classe.
+        <ul>
+            <?php
+            foreach ($classRoom as $value) {
+                ?>
+                <li><a href="classe-<?= $value["id"] ?>.html"> <?= $value["name"] ?></a> - <?= $value["comment"] ?></li>
+                <?php
+            }
+            ?>
+        </ul>
+    </div>
+    <?php
+    // include("footer.php");
+} else {
+    header("Location: index.php");
+}
+?>
