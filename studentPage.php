d224a6b4beea6a5b13c45335266dee9fa938ff36
Quentin Helleboid <Quentin@MacBook-Air-de-Quentin.local>
2017-01-06 09:49:09 +0100

    Ajout objet etudiant

--- studentPage.php
+++ studentPage.php
@@ -2,6 +2,8 @@
 include("header.php");
 
 if (isset($_SESSION['connected_id']) && !empty($_SESSION['connected_id']) && $_SESSION['connected_type'] == "student") {
+
+    $person = unserialize($_SESSION['connected_objet']);
     ?>
     Page d'un eleve
 
